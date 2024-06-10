0628a25668eae1167bf3a61dcb323db3750a481b
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-06-10 18:34:02 +0300

    feat: flowrouter added

---
+++ client/router/index.js
@@ -0,0 +1,20 @@
+import { FlowRouter } from "meteor/ostrio:flow-router-extra";
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+import "../pages/Home/index.html";
+import "../pages/NotFound/index.html";
+
+// Home
+FlowRouter.route('/', {
+  name: 'home',
+  action() {
+    BlazeLayout.render('app-layout', { content: 'home' });
+  },
+});
+
+// 404
+FlowRouter.route('*', {
+  action() {
+    BlazeLayout.render('notFound-layout', { content: 'notFound' });
+  },
+});
