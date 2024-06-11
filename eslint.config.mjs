0f3b73ba1df78606749e1167609c52eb19b636ea
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-06-11 11:16:40 +0300

    feat: eslint prettier added

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+]
